dae00aa7d872ab532388be3302da4bc6563d1dbf
GAURAV KUMAR SAINI <108860461+Gauravkumar2410@users.noreply.github.com>
2024-12-20 15:41:32 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-3xl flex justify-center align-center font-normal leading-normal mt-5 mb-2 text-red-400">
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
